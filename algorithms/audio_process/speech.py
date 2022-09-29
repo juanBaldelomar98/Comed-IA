6d404f02928a29680962a72660dd51519c9cc1e2
Juan José Baldelomar Cabrera <52798296+juanBaldelomar98@users.noreply.github.com>
2022-09-29 01:22:01 -0500

    Added speech recognition

--- algorithms/audio_process/speech.py
+++ algorithms/audio_process/speech.py
@@ -1,5 +1,9 @@
 """Script that handles the speech in the audio"""
+from algorithms.speech.recognition import get_large_audio_transcription
+from algorithms.speech.script_match import matching
 
 
 def process_speech(audio_path: str, script_path: str) -> tuple:
-    return "", "", ""
+    transcript, intervals = get_large_audio_transcription(audio_path)
+    transcript, intervals, script = matching(transcript, intervals, script_path)
+    return transcript, intervals, script
---
+++ algorithms/speech/script_match.py
@@ -0,0 +1,5 @@
+"""Script that matches transcript obtained from audio to the scripted show"""
+
+
+def matching(transcript: list, intervals: list, script_path: str) -> tuple:
+    return "", "", ""
